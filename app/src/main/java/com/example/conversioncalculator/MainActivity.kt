18c414a0841d9a8a3a7631192384fac4b5cae98b
Allison Bickford <abickford@michiganlabs.com>
2019-10-11 11:22:23 -0400

    settings menu + other features

--- app/src/main/java/com/example/conversioncalculator/MainActivity.kt
+++ app/src/main/java/com/example/conversioncalculator/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.conversioncalculator
 
 import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,14 +12,17 @@ import android.view.MenuItem
 import android.widget.TextView
 import com.example.conversioncalculator.UnitsConverter.*
 import android.content.Intent
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    var isLength = true
-    var fromLenUnits = LengthUnits.Yards
-    var toLenUnits = LengthUnits.Meters
-    var fromVolUnits = VolumeUnits.Gallons
-    var toVolUnits = VolumeUnits.Liters
+    private var isLength = true
+    private var fromLenUnits = LengthUnits.Yards
+    private var toLenUnits = LengthUnits.Meters
+    private var fromVolUnits = VolumeUnits.Gallons
+    private var toVolUnits = VolumeUnits.Liters
+    private val SETTINGS_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +34,27 @@ class MainActivity : AppCompatActivity() {
         val modeButton = findViewById<Button>(R.id.modeButton)
 
         // set listeners for buttons
-        clearButton.setOnClickListener { clearFields() }
-        calcButton.setOnClickListener { if (isLength) calculateLen() else calculateVol() }
-        modeButton.setOnClickListener { changeMode() }
+        clearButton.setOnClickListener {
+            clearFields()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(fromInput.windowToken, 0)
+        }
+        calcButton.setOnClickListener {
+            calculate()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(fromInput.windowToken, 0)
+        }
+        modeButton.setOnClickListener {
+            changeMode()
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(fromInput.windowToken, 0)
+        }
 
+
+        val fromInput = findViewById<EditText>(R.id.fromInput)
+        val toInput = findViewById<EditText>(R.id.toInput)
+        fromInput.setOnFocusChangeListener { _, _ -> toInput.text.clear() }
+        toInput.setOnFocusChangeListener { _, _ -> fromInput.text.clear() }
     }
 
     private fun clearFields() {
@@ -44,107 +65,36 @@ class MainActivity : AppCompatActivity() {
         toInput.text.clear()
     }
 
-    private fun calculateLen() {
-       var enteredVal = 0.0
+
+    private fun calculate() {
         val fromInput = findViewById<EditText>(R.id.fromInput)
         val toInput = findViewById<EditText>(R.id.toInput)
 
-        // If fromInput empty or both input fields empty- display error message.
-        if(fromInput.text.toString() == ""  || ( fromInput.text.toString() == ""
-                        && toInput.text.toString() == "")) {
+        // If both input fields empty - display error message.
+        if(fromInput.text.isEmpty() && toInput.text.isEmpty()) {
             AlertDialog.Builder(this).setTitle("Error Message").
-                    setMessage("From Input Empty!")
-                    .setPositiveButton("OK") { dialog, _->
-                        //clearFields()
-                        dialog.dismiss()
-                    }.create().show()
+                setMessage("Please enter a value to calculate.")
+                .setPositiveButton("OK") { dialog, _->
+                    //clearFields()
+                    dialog.dismiss()
+                }.create().show()
 
         }
 
-        // If toInput empty, or both are not empty only use FromInput
-        if(toInput.text.toString() == "" || (fromInput.text.toString() != "" &&
-                        toInput.text.toString() != "") ){
-            enteredVal = (fromInput.text.toString()).toDouble()
+        val fieldToRead = if (fromInput.text.isNotEmpty()) fromInput else toInput
+        val fieldToPopulate = if (fromInput.text.isEmpty()) fromInput else toInput
 
-
+        if (isLength) {
+            val fromUnits = if (fieldToRead == fromInput) fromLenUnits else toLenUnits
+            val toUnits = if (fromUnits == fromLenUnits) toLenUnits else fromLenUnits
+            fieldToPopulate.setText(
+                convert(fieldToRead.text.toString().toDouble(), fromUnits, toUnits).toString())
+        } else {
+            val fromUnits = if (fieldToRead == fromInput) fromVolUnits else toVolUnits
+            val toUnits = if (fromUnits == fromVolUnits) toVolUnits else fromVolUnits
+            fieldToPopulate.setText(
+                convert(fieldToRead.text.toString().toDouble(), fromUnits, toUnits).toString())
         }
-
-        // Length Conversion
-        if(isLength) {
-            var first: LengthUnits = LengthUnits.Meters
-            var sec: LengthUnits = LengthUnits.Meters
-
-            when {
-                fromLenUnits.toString() == "Meters" -> first = LengthUnits.Meters
-                fromLenUnits.toString() == "Yards" -> first = LengthUnits.Yards
-                fromLenUnits.toString() == "Miles" -> first = LengthUnits.Miles
-            }
-
-            when {
-
-                toLenUnits.toString() == "Meters" -> sec = LengthUnits.Meters
-                toLenUnits.toString() == "Yards" -> sec = LengthUnits.Yards
-                toLenUnits.toString() == "Miles" -> sec = LengthUnits.Miles
-            }
-
-            toInput.setText(convert(
-                    enteredVal,
-                    LengthUnits.valueOf(fromLenUnits.toString()),
-                    LengthUnits.valueOf(toLenUnits.toString())).toString())
-        }
-
-
-    }
-
-    private fun calculateVol() {
-        var enteredVal = 0.0
-        val fromInput = findViewById<EditText>(R.id.fromInput)
-        val toInput = findViewById<EditText>(R.id.toInput)
-
-        // If fromInput empty or both input fields empty- display error message.
-        if(fromInput.text.toString() == ""  || ( fromInput.text.toString() == ""
-                        && toInput.text.toString() == "")) {
-            AlertDialog.Builder(this).setTitle("Error Message").
-                    setMessage("From Input Empty!")
-                    .setPositiveButton("OK") { dialog, _->
-                        //clearFields()
-                        dialog.dismiss()
-                    }.create().show()
-
-        }
-
-        // If toInput empty, or both are not empty only use FromInput
-        if(toInput.text.toString() == "" || (fromInput.text.toString() != "" &&
-                        toInput.text.toString() != "") ){
-            enteredVal = (fromInput.text.toString()).toDouble()
-
-
-        }
-
-        // Volume Conversion
-        if(!isLength) {
-            var first: VolumeUnits = VolumeUnits.Liters
-            var sec: VolumeUnits = VolumeUnits.Liters
-
-            when {
-                fromVolUnits.toString() == "Liters" -> first = VolumeUnits.Liters
-                fromVolUnits.toString() == "Gallons" -> first = VolumeUnits.Gallons
-                fromVolUnits.toString() == "Quarts" -> first = VolumeUnits.Quarts
-            }
-
-            when {
-
-                toVolUnits.toString() == "Liters" -> sec = VolumeUnits.Liters
-                toVolUnits.toString() == "Gallons" -> sec = VolumeUnits.Gallons
-                toVolUnits.toString() == "Quarts" -> sec = VolumeUnits.Quarts
-            }
-
-            toInput.setText(convert(
-                    enteredVal,
-                    VolumeUnits.valueOf(fromVolUnits.toString()),
-                    VolumeUnits.valueOf(toVolUnits.toString())).toString())
-        }
-
     }
 
     private fun changeMode() {
@@ -152,6 +102,7 @@ class MainActivity : AppCompatActivity() {
         val title = findViewById<TextView>(R.id.titleLabel)
         val fromUnits = findViewById<TextView>(R.id.fromUnits)
         val toUnits = findViewById<TextView>(R.id.toUnits)
+        findViewById<EditText>(R.id.toInput).text.clear()
         if (isLength) {
             title.text = resources.getText(R.string.lengthTitle)
             fromUnits.text = fromLenUnits.name
@@ -163,27 +114,73 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    override fun onCreateOptionsMenu(menu:Menu){
-//        super.onCreateOptionsMenu(menu)
-//        getMenuInflater.inflate(R.menu.settings, menu)
-//        return true;
-//
-//    }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        if (item.itemId == R.id.settings){
-//            val i = Intent(MainActivity.this, Main2Activity::class.java)
-//            i.putExtra("Mode", isLength)
-//            startActivityForResult(i, VICE_SELECT)
-//        }
-//        return true
-//    }
-//
-//    override fun onActivityResult(requestCode: int, resultCode: int, data: Intent) {
-//        clearFields()
-//        if(resultCode == Activity.RESULT_OK) {
-//            fromUnit.setText(data.getStringExtra("FromUnit")
-//            toUnit.setText(data.getStringExtra(ToUnit))
-//        }
-//    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_settings, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings_action -> {
+                navigateToSettings()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navigateToSettings() {
+        val intent = Intent(this, SettingsActivity::class.java).apply {
+            if (isLength) {
+                putExtra("IS_LENGTH", true)
+                putExtra("FROM_UNITS", fromLenUnits.name)
+                putExtra("TO_UNITS", toLenUnits.name)
+            } else {
+                putExtra("IS_LENGTH", false)
+                putExtra("FROM_UNITS", fromVolUnits.name)
+                putExtra("TO_UNITS", toVolUnits.name)
+            }
+        }
+        startActivityForResult(intent, SETTINGS_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SETTINGS_REQUEST_CODE) {
+            clearFields()
+            if (resultCode == Activity.RESULT_OK) {
+                val intentFromUnits = data?.getStringExtra("FROM_UNITS")
+                val intentToUnits = data?.getStringExtra("TO_UNITS")
+                val fromUnits = findViewById<TextView>(R.id.fromUnits)
+                val toUnits = findViewById<TextView>(R.id.toUnits)
+                if (isLength) {
+                    if (intentFromUnits != null) {
+                        val unitString = LengthUnits.valueOf(intentFromUnits)
+                        fromLenUnits = unitString
+                        fromUnits.text = fromLenUnits.name
+                    }
+
+                    if (intentToUnits != null) {
+                        val unitString = LengthUnits.valueOf(intentToUnits)
+                        toLenUnits = unitString
+                        toUnits.text = toLenUnits.name
+                    }
+                } else {
+                    if (intentFromUnits != null) {
+                        val unitString = VolumeUnits.valueOf(intentFromUnits)
+                        fromVolUnits = unitString
+                        fromUnits.text = fromVolUnits.name
+                    }
+
+                    if (intentToUnits != null) {
+                        val unitString = VolumeUnits.valueOf(intentToUnits)
+                        toVolUnits = unitString
+                        toUnits.text = toVolUnits.name
+                    }
+                }
+            }
+        }
+    }
 }
